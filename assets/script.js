f4a365240b7b2a8bedd26c1845a5bf02704a3544
micromanyth <miromanyth@gmail.com>
2017-12-30 09:52:17 -0500

    finished up onscroll event for counter

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,7 @@ var happy = document.querySelector("#happy");
 var working = document.querySelector("#working");
 var projects = document.querySelector("#projects");
 var awards = document.querySelector("#awards");
+var counter = document.querySelector(".counter");
 
 function animateValue(id, start, end, duration) {
     var range = end - start;
@@ -33,8 +34,21 @@ function animateValue(id, start, end, duration) {
     }, stepTime);
 }
 
+function startCount() {
+	animateValue("happy", 0, 15, 500);
+	animateValue("working", 0, 325, 500);
+	animateValue("projects", 0, 19, 500);
+	animateValue("awards", 0, 13, 500);	
+}
 
-animateValue("happy", 0, 15, 500);
-animateValue("working", 0, 325, 500);
-animateValue("projects", 0, 19, 500);
-animateValue("awards", 0, 13, 500);
+function isInViewport(elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+window.onscroll = function() {if (isInViewport(counter)) {startCount();}}
